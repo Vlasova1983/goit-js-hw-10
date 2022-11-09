5d513f2160a3411a500ce156585528fe6e8ea52c
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-09 19:57:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import API from './fetchCountries';
+// import API from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
 const inputEl = document.querySelector('#search-box');
-inputEl.addEventListener ('input', countrySearch);
+inputEl.addEventListener ('input', onInput);
 
-function countrySearch() {   
-    fetchCountries(inputEl.value) ;
-};
+function onInput(){
+    fetchCountries(inputEl.value);
+} 
 
 function fetchCountries(name){    
     fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`)
@@ -18,33 +17,33 @@ function fetchCountries(name){
         return response.json();
     })
     .then(country=>{
-        makeFieldsCountry(country)                 
+        if (country.length>10){
+            const markup=country.map(item=>``)    
+            countryList.innerHTML=markup.join('');  
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } 
+        else if (country.length<2){  
+            const markup=country.map(item=>
+                `<div class="contener">   
+                    <img class="img" src="${item.flags.svg}" alt="${item.name}"/>
+                    <p class="">${item.name}</p>
+                </div>
+                <p class="">Capital: ${item.capital}</p>
+                <p class="">Population: ${item.population}</p>
+                <p class="">Languages:  ${item.languages[0].nativeName}</p>`)    
+            countryList.innerHTML=markup.join('');        
+        } 
+        else {
+            const markup=country.map(item=>
+                `<div class="flex"> 
+                    <img class="img" src="${item.flags.svg}" alt="${item.name}"/>
+                    <p class="">${item.name}</p> 
+                </div>`)    
+            countryList.innerHTML=markup.join('');        
+        }
     })
     .catch(error=>{
+        countryList.classList.add('clear');
         Notiflix.Notify.failure('Oops, there is no country with that name');
     })
 } 
- 
- 
-function makeFieldsCountry(country){
-    if (country.length>10){
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    } else if (country.length<2){
-        const a =  `<div class="contener">   
-                        <img class="img" src="${country[0].flags.svg}" alt="${country[0].name}"/>
-                        <p class="">${country[0].name}</p>
-                    </div>
-                    <p class="">Capital: ${country[0].capital}</p>
-                    <p class="">Population: ${country[0].population}</p>
-                    <p class="">Languages:  ${country[0].languages[0].nativeName}</p>  
-                    ` 
-        countryInfo.insertAdjacentHTML('beforeend', a);
-    } else {
-        const a = `<img class="img" src="${country[0].flags.svg}" alt="${country[0].name}"/>
-                    <p class="">${country[0].name}</p> 
-                    ` 
-                    console.log(a);
-        countryList.insertAdjacentHTML('beforeend', a); 
-      }
-}
-    
