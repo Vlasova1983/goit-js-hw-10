8df7b71dd387d9111ab4fcd32f22689d4a022c9b
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-10 16:21:10 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -8,12 +8,11 @@ function fetchCountries(name){
     })
     .then(country=>{
         if (country.length>10){
-            const markup=country.map(item=>``)    
-            countryList.innerHTML=markup.join('');  
+            document.querySelector('.country-list').innerHTML=country.map(item=>``).join('');  
             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } 
         else if (country.length<2){  
-            const markup=country.map(item=>
+            document.querySelector('.country-list').innerHTML = country.map(item=>
                 `<div class="contener">   
                     <img class="img" src="${item.flags.svg}" alt="${item.name}"/>
                     <p class="">${item.name}</p>
@@ -21,20 +20,22 @@ function fetchCountries(name){
                 <p class="">Capital: ${item.capital}</p>
                 <p class="">Population: ${item.population}</p>
                 <p class="">Languages:  ${item.languages[0].nativeName}</p>`)    
-            countryList.innerHTML=markup.join('');        
+            .join('');        
         } 
         else {
-            const markup=country.map(item=>
+            document.querySelector('.country-list').innerHTML=country.map(item=>
                 `<div class="flex"> 
                     <img class="img" src="${item.flags.svg}" alt="${item.name}"/>
                     <p class="">${item.name}</p> 
                 </div>`)    
-            countryList.innerHTML=markup.join('');        
+            .join('');        
         }
     })
     .catch(error=>{
+        document.querySelector('.country-list').classList.add('clear');
         Notiflix.Notify.failure('Oops, there is no country with that name');
     })
 } 
+
 
 export default {fetchCountries};
