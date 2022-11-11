39a0a1323411cd6315e037d7ee06d926797889e8
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-11 11:22:20 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -7,11 +7,12 @@ function fetchCountries(name){
         return response.json();
     })
     .then(country=>{
+        console.log(country); 
         if (country.length>10){
             document.querySelector('.country-list').innerHTML=country.map(item=>``).join('');  
             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } 
-        else if (country.length<2){  
+        else if (country.length<2){             
             document.querySelector('.country-list').innerHTML = country.map(item=>
                 `<div class="contener">   
                     <img class="img" src="${item.flags.svg}" alt="${item.name}"/>
@@ -19,8 +20,8 @@ function fetchCountries(name){
                 </div>
                 <p class="">Capital: ${item.capital}</p>
                 <p class="">Population: ${item.population}</p>
-                <p class="">Languages:  ${item.languages[0].nativeName}</p>`)    
-            .join('');        
+                <p class="">Languages: ${item.languages.map(item=>`${item.name}, `).join('')}</p>`)    
+            .join('');                   
         } 
         else {
             document.querySelector('.country-list').innerHTML=country.map(item=>
